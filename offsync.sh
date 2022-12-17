ee2f407ed3bdf16c4148e44b83583c741b1fa922
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-12-17 12:24:16 +0530

    error handling

--- offsync.sh
+++ offsync.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-python3 /opt/offsync/main.py "$@"
+# Check if /opt/offsync/ directory does not exist
+if ! [ -d /opt/offsync/ ]; then
+  echo -e "\033[1;31mError: Offsync not installed.\033[0m" >&2
+  exit 1
+fi
 
-exit
+# Check if /opt/offsync/main.py file does not exist
+if ! [ -f /opt/offsync/main.py ]; then
+  echo -e "\033[1;31mError: Failed to start offsync, '/opt/offsync/main.py' not found.\033[0m" >&2
+  exit 1
+fi
+
+python3 /opt/offsync/main.py "$@"
