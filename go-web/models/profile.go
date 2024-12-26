9bea17a54110ba13272017b28ddf51181c97bff3
FlareXes <70066527+FlareXes@users.noreply.github.com>
2024-12-26 09:23:24 +0530

    chore: remove go-web, dropped the idea.

--- go-web/models/profile.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Profile struct {
-	Site     string `form:"site"`
-	Username string `form:"username"`
-	Secret   string `form:"secret"`
-	Counter  string `form:"counter"`
-	Length   int    `form:"length"`
-}
